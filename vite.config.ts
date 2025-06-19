d14c341803fd6958b0831e969399d1e8f4758e5f
Kirubaharan <kirubakrishkk@gmail.com>
2025-06-19 11:43:50 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/kiruba-portfolio/",
+  
   server: {
     host: "::",
     port: 8080,
